0d726da539937f0d4fb808c7986c6652231bfd75
ricky-696 <ricky321027@gmail.com>
2023-07-16 23:30:47 +0800

    update

--- Track1/train_YOLOv8.py
+++ Track1/train_YOLOv8.py
@@ -2,15 +2,21 @@ from ultralytics import YOLO
 
     
 if __name__ == '__main__':
+    # Parameter
     pretrain_weight = '/home/Ricky/0_Project/ROADpp_challenge_ICCV2023/Pretrain/yolov8l.pt'
-    model = YOLO(model=pretrain_weight)
+    imgsz = [1280, 1280]
+    batch_size = 8
+    name = 'yolov8l_T1_' + str(imgsz[1]) + '_batch_' + str(batch_size) + '_'
 
     # Training.
+    model = YOLO(model=pretrain_weight)
+
     results = model.train(
     data = '/home/Ricky/0_Project/ROADpp_challenge_ICCV2023/Track1/track1.yaml',
-    imgsz = 1280,
+    imgsz = imgsz,
+    rect = True, # if input not square set this true
     device = 0,
     epochs = 50,
-    batch = 8,
-    name = 'yolov8l_T1_1280_FL_batch_8_'
+    batch = batch_size,
+    name = name
     )
